1ae1e21db1429939e062a1c761d00baf17dcb2b9
Federico Rota <federico.rota01@gmail.com>
2018-11-15 00:45:16 +0100

    First local components

---
+++ packages/link/src/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export default function Link({ className, children, ...props }) {
+  return (
+    <a className={className} href="#" {...props}>
+      My link {children}
+    </a>
+  );
+}
